f23abc22151a016c39e8c4eabb7f405f59f35749
Sebastien Mirolo <smirolo@djaodjin.com>
2022-04-30 15:52:13 -0700

    adds missing import

--- testsite/settings.py
+++ testsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import re, os, sys
+import logging, re, os, sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 APP_NAME = os.path.basename(BASE_DIR)
 RUN_DIR = os.getcwd()
